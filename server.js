0b04dba571c927695b9d19a67e37e1e11211cf1d
sanketkava <sanketkava555@gmail.com>
2025-04-19 21:56:43 +0530

    fix

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { body, validationResult } = require('express-validator');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://192.168.x.x:3000'], // Update with your network IP
+  origin: ['*'], // Update with your network IP
 }));
 app.use(express.json());
 
@@ -17,7 +19,7 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/appfinity', {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
